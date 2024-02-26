419015514f691ad82f5cd1330b122d3f34b23d95
ppedvLukasK <lukask@ppedv.de>
2024-02-26 16:19:02 +0100

    M004

---
+++ M004/FourValueToMarginConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace M004;
+
+internal class FourValueToMarginConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		//Hier kommen 4 Werte per object[] hinein
+		return new Thickness((double) values[0], (double) values[1], (double) values[2], (double) values[3]);
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+	{
+		return null;
+	}
+}
---
+++ M004/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace M004;
+
+public partial class MainWindow : Window
+{
+	//Normales C# Property
+	public int Alter { get; set; }
+
+	//C# Property + DependencyProperty darunter
+	public int Zaehler
+	{
+		get { return (int) GetValue(ZaehlerProperty); }
+		set { SetValue(ZaehlerProperty, value); }
+	}
+
+	// Using a DependencyProperty as the backing store for Zaehler.  This enables animation, styling, binding, etc...
+	public static readonly DependencyProperty ZaehlerProperty =
+		DependencyProperty.Register("Zaehler", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		BindingExpression ex = Eingabe.GetBindingExpression(TextBox.TextProperty);
+		ex.UpdateTarget();
+	}
+}
