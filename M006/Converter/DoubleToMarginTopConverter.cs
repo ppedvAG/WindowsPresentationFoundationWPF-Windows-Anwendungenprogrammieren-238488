638491e878856ddd32bcbe01a0a041dd407d913c
ppedvLukasK <lukask@ppedv.de>
2024-02-27 14:45:39 +0100

    M006

---
+++ M006/Converter/DoubleToMarginTopConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace M006.Converter;
+
+public class DoubleToMarginTopConverter : IValueConverter
+{
+	/// <summary>
+	/// Source -> Target
+	/// value: Wert, der vom Slider an den Button gesendet wird
+	/// Rückgabewert: Der Wert, der auf der anderen Seite ankommen soll
+	/// </summary>
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return new Thickness(0, (double) value, 0, 0); //Dieser Wert wird von dem Binding auf dem Button empfangen
+	}
+
+	//Ziel -> Quelle, kann hier leer bleiben
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return null;
+	}
+}
---
+++ M006/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M006;
+
+public partial class MainWindow : Window
+{
+	public static double Test { get; set; } = 100.0;
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		Wochentage.ItemsSource = Enum.GetValues<DayOfWeek>();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Resources["FontSize"] = 60.0;
+	}
+}
