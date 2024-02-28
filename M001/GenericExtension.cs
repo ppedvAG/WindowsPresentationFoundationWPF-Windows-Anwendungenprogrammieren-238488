6a201b7e865cf7aea1f1b5c55a02f552599bae5e
ppedvLukasK <lukask@ppedv.de>
2024-02-28 12:30:07 +0100

    M001

---
+++ M001/GenericExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Markup;
+
+namespace M001;
+
+public class GenericExtension : MarkupExtension
+{
+	private object[] values;
+
+	public Type OriginalType { get; set; }
+
+	public Type WrapperType { get; set; }
+
+	public override object ProvideValue(IServiceProvider serviceProvider)
+	{
+		return values ??= OriginalType.GetType()
+			.GetProperties()
+			.Select(e => Activator.CreateInstance(WrapperType, e))
+			.ToArray();
+	}
+}
